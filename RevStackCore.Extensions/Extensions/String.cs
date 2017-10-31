45850c4b69644f480092f08ffcfc382c193dda9e
Scott <sfrancis@misinteractive.com>
2017-10-31 00:14:37 -0400

    Added method ToFullMonthName

--- RevStackCore.Extensions/Extensions/String.cs
+++ RevStackCore.Extensions/Extensions/String.cs
@@ -376,6 +376,57 @@ namespace RevStackCore.Extensions
 			return month;
 		}
 
+        /// <summary>
+        /// Tos the name of the month.
+        /// </summary>
+        /// <returns>The month name.</returns>
+        /// <param name="src">Source.</param>
+        public static string ToFullMonthName(this int src)
+        {
+            string month = "";
+            switch (src)
+            {
+                case 1:
+                    month = "January";
+                    break;
+                case 2:
+                    month = "February";
+                    break;
+                case 3:
+                    month = "March";
+                    break;
+                case 4:
+                    month = "April";
+                    break;
+                case 5:
+                    month = "May";
+                    break;
+                case 6:
+                    month = "June";
+                    break;
+                case 7:
+                    month = "July";
+                    break;
+                case 8:
+                    month = "August";
+                    break;
+                case 9:
+                    month = "September";
+                    break;
+                case 10:
+                    month = "October";
+                    break;
+                case 11:
+                    month = "November";
+                    break;
+                case 12:
+                    month = "December";
+                    break;
+            }
+
+            return month;
+        }
+
 	}
 }
 
